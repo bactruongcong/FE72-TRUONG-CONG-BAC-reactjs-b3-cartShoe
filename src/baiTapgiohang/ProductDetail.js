8b34ea7144485baa190c43018dfb319e9d68620f
huong_nam <huong_nam@gmail.com>
2022-07-28 15:55:19 +0700

    fix modal close

--- src/baiTapgiohang/ProductDetail.js
+++ src/baiTapgiohang/ProductDetail.js
@@ -12,7 +12,7 @@ class ProductDetail extends Component {
             <div className="modal-content">
                 <div className="modal-header">
                 <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
                 </div>
